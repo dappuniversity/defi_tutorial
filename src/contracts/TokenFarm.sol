391f515c1bfd073137481ff3bed79f17e0c1d319
gwmccubbin <gwmccubbin@gmail.com>
2020-08-18 14:01:20 -0500

    Part 2

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,6 +1,15 @@
 pragma solidity ^0.5.0;
 
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
 contract TokenFarm {
-    // All code goes here...
     string public name = "Dapp Token Farm";
+    DappToken public dappToken;
+    DaiToken public daiToken;
+
+    constructor(DappToken _dappToken, DaiToken _daiToken) public {
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+    }
 }
---
+++ test/TokenFarm.test.js
@@ -0,0 +1,55 @@
+const DaiToken = artifacts.require('DaiToken')
+const DappToken = artifacts.require('DappToken')
+const TokenFarm = artifacts.require('TokenFarm')
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .should()
+
+function tokens(n) {
+  return web3.utils.toWei(n, 'ether');
+}
+
+contract('TokenFarm', ([owner, investor]) => {
+  let daiToken, dappToken, tokenFarm
+
+  before(async () => {
+    // Load Contracts
+    daiToken = await DaiToken.new()
+    dappToken = await DappToken.new()
+    tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address)
+
+    // Transfer all Dapp tokens to farm (1 million)
+    await dappToken.transfer(tokenFarm.address, tokens('1000000'))
+
+    // Send tokens to investor
+    await daiToken.transfer(investor, tokens('100'), { from: owner })
+  })
+
+  describe('Mock DAI deployment', async () => {
+    it('has a name', async () => {
+      const name = await daiToken.name()
+      assert.equal(name, 'Mock DAI Token')
+    })
+  })
+
+  describe('Dapp Token deployment', async () => {
+    it('has a name', async () => {
+      const name = await dappToken.name()
+      assert.equal(name, 'DApp Token')
+    })
+  })
+
+  describe('Token Farm deployment', async () => {
+    it('has a name', async () => {
+      const name = await tokenFarm.name()
+      assert.equal(name, 'Dapp Token Farm')
+    })
+
+    it('contract has tokens', async () => {
+      let balance = await dappToken.balanceOf(tokenFarm.address)
+      assert.equal(balance.toString(), tokens('1000000'))
+    })
+  })
+
+})
