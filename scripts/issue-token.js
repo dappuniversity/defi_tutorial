ce564bf827fcaccec9b6deb19f98527a3858d7d6
Maryam <maryam@Maryams-MacBook-Air.local>
2022-03-26 19:46:19 +0430

    new script issue token

--- scripts/issue-token.js
+++ scripts/issue-token.js
@@ -1,7 +1,7 @@
-const TokenFarm = artifacts.require('TokenFarm')
+const Farm = artifacts.require('Farm')
 
 module.exports = async function(callback) {
-  let tokenFarm = await TokenFarm.deployed()
+  let tokenFarm = await Farm.deployed()
   await tokenFarm.issueTokens()
   // Code goes here...
   console.log("Tokens issued!")
